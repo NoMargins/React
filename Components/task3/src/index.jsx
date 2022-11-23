ee8ee6482ca47b5d15fb0b14530872efe104ddb3
Olena Ryabenko <olenaryabenko@gmail.com>
2022-11-23 11:34:16 +0200

    headache

--- Components/task3/src/index.jsx
+++ Components/task3/src/index.jsx
@@ -11,4 +11,4 @@ const user= {
     avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROzP7f_N_r-tJ6zgSOsylbKlGUUIGrs8jNjQ&usqp=CAU"
 }
 
-ReactDOM.render(<Comment text="You can do it!" user={user} date={new Date()}/>, rootElem);
+ReactDOM.render(<Comment text="You can do it!" author={user} date={new Date()}/>, rootElem);
