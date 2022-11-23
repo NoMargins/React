4957211240b0246573b05f37b118fb6421baf70d
Olena Ryabenko <olenaryabenko@gmail.com>
2022-11-23 12:46:35 +0200

    ok

--- State/task1/src/App.jsx
+++ State/task1/src/App.jsx
@@ -5,8 +5,9 @@ import "./index.scss";
 const App = () => {
     return (
         <>
-        <Counter start={18} interval={1000}/>
-        <Counter start={-50} interval={500}/>
+        <Counter start={5} interval={1000}/>
+        <Counter start={12} interval={500}/>
+        <Counter start={-21} interval={500}/>
         </>
     )
 }
