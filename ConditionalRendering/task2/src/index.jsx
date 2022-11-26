71e1244a129affe51dd0ca5101f7787f669b9d46
Olena Ryabenko <olenaryabenko@gmail.com>
2022-11-27 00:20:43 +0200

    initial commit

--- ConditionalRendering/task2/src/index.jsx
+++ ConditionalRendering/task2/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Mailbox from "./Mailbox.jsx";
 
 const rootElem = document.querySelector("#root");
-
-ReactDOM.render(</>, rootElem);
+ReactDOM.render(<Mailbox unreadMessages={[2, 3]}/>, rootElem);
