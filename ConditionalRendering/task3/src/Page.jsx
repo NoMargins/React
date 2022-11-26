4c3f3096f068766719ab7afbf88cb13a6b596d58
Olena Ryabenko <olenaryabenko@gmail.com>
2022-11-27 01:04:58 +0200

    text changes

--- ConditionalRendering/task3/src/Page.jsx
+++ ConditionalRendering/task3/src/Page.jsx
@@ -18,10 +18,10 @@ class Page extends React.Component {
         <div className="page">
         <Message text={this.state.text}/>
         <div className="actions">
-        <button className="btn" onClick={() => this.setText("Hello, world")}>
+        <button className="btn" onClick={() => this.setText("Hello, world!")}>
         Text 1
         </button>
-        <button className="btn" onClick={() => this.setText("Another exciting text")}>
+        <button className="btn" onClick={() => this.setText("Another exciting text.")}>
         Text 2
         </button>
         <button className="btn" onClick={() => this.setText("")}>
