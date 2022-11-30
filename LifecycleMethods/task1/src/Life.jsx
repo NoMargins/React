348f40a592f1bb3f58aa51829b6c4791c527e673
Olena Ryabenko <olenaryabenko@gmail.com>
2022-11-30 12:16:29 +0200

    initial commit

---
+++ LifecycleMethods/task1/src/Life.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './styles.scss';
+
+class Life extends React.Component {
+	constructor(props) {
+		super(props);
+		console.log('constructor: good plase to create state');
+	}
+
+	componentDidMount() {
+		console.log('componentDidMount: API calls, subscriptions');
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log(
+			'shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
+		);
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log(
+			'componentDidUpdate(prevProps, prevState): some updates based on new props'
+		);
+	}
+
+	componentWillUnmount() {
+		console.log(
+			'componentWillUnmount: clean before DOM related to component will be removed'
+		);
+	}
+
+	render() {
+		console.log('return React element to build DOM');
+		return <div>I'm learning Lifecycle methods of React</div>;
+	}
+}
+
+export default Life;
