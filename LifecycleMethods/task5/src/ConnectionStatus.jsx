768234c6d51a2037d984934fe063428820c6a4bc
Olena Ryabenko <olenaryabenko@gmail.com>
2022-11-30 16:24:20 +0200

    second commit

--- LifecycleMethods/task5/src/ConnectionStatus.jsx
+++ LifecycleMethods/task5/src/ConnectionStatus.jsx
@@ -3,29 +3,45 @@ import './styles.scss';
 
 class ConnectionStatus extends React.Component {
 	state = {
-		status: 'online',
+		status: window.ononline ? 'online' : 'offline',
 	};
 
-	offlineFunc = () => {
-		if (window.onoffline) {
+	offlineFunc = (e) => {
+		if (e.target.onoffline) {
 			this.setState({
 				status: 'offline',
 			});
-		document.querySelector('status').classList.add('status_offline');
 		}
 	};
-    
+
+	onlineFunc = (e) => {
+		if (e.target.ononline) {
+			this.setState({
+				status: 'online',
+			});
+		}
+	};
+
 	componentDidMount() {
 		window.addEventListener('offline', this.offlineFunc);
+		window.addEventListener('online', this.onlineFunc);
 	}
 
 	componentWillUnmount() {
 		window.removeEventListener('offline', this.offlineFunc);
-		document.querySelector('status').classList.remove('status_offline');
+		window.removeEventListener('online', this.onlineFunc);
 	}
 
 	render() {
-		return <div className='status'>{this.state.status}</div>;
+		return (
+			<div
+				className={
+					this.state.status === 'offline' ? 'status status_offline' : 'status'
+				}
+			>
+				{this.state.status}
+			</div>
+		);
 	}
 }
 
