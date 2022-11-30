2ec5cf1f7bfd725775120d81c6a23b2b041f419c
Olena Ryabenko <olenaryabenko@gmail.com>
2022-11-30 12:26:09 +0200

    ok

--- LifecycleMethods/task1/src/Life.jsx
+++ LifecycleMethods/task1/src/Life.jsx
@@ -17,7 +17,7 @@ class Life extends React.Component {
 		);
 	}
 
-	componentDidUpdate() {
+	componentDidUpdate(prevProps, prevState) {
 		console.log(
 			'componentDidUpdate(prevProps, prevState): some updates based on new props'
 		);
@@ -25,7 +25,7 @@ class Life extends React.Component {
 
 	componentWillUnmount() {
 		console.log(
-			'componentWillUnmount(): clean before DOM related to component will be removed'
+			'componentWillUnmount(): cleanup before DOM related to component will be removed'
 		);
 	}
 
