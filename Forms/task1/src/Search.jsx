94afaa96998cbc75262b0e02eb64f0531d1dd519
Olena Ryabenko <olenaryabenko@gmail.com>
2022-12-02 21:06:51 +0200

    ok

--- Forms/task1/src/Search.jsx
+++ Forms/task1/src/Search.jsx
@@ -12,21 +12,21 @@ class Search extends React.Component {
 		});
 	};
 
-	handleSubmit = (e) => {
+	search = (e) => {
 		e.preventDefault();
 		alert(`Search text: ${this.state.value}`);
 	};
 
 	render() {
 		return (
-			<form class='search'>
+			<form className='search' onSubmit={this.search}>
 				<input
 					type='text'
 					className='search__input'
 					value={this.state.value}
 					onChange={this.handleValue}
 				/>
-				<button className='search__button' onClick={this.handleSubmit}>
+				<button className='search__button' type='submit'>
 					Search
 				</button>
 			</form>
