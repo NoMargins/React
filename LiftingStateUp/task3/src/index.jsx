1e98595ba2d44f5bf9bf6eec33cc9c2ff9c5ca48
Olena Ryabenko <olenaryabenko@gmail.com>
2022-12-06 14:53:14 +0200

    ok

--- LiftingStateUp/task3/src/index.jsx
+++ LiftingStateUp/task3/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import Calculator from './Calculator.jsx';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElem);
+
+ReactDOM.render(<Calculator />, rootElem);
