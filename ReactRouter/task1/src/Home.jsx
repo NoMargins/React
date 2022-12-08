b5b3ec4ebd9ef9a218780aaf3e9dddb1b6166853
Olena Ryabenko <olenaryabenko@gmail.com>
2022-12-08 19:15:06 +0200

    initial commit

--- ReactRouter/task1/src/Home.jsx
+++ ReactRouter/task1/src/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	return (
-		<div class='page__content'>
+		<div className='page__content'>
 			<h1>🏠</h1>
-			<a href='/products'>To products</a>
+			<Link to='/products'>To products</Link>
 		</div>
 	);
 };
